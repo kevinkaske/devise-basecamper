ab812457aff6c5caa209cf18f35dda3eefd8de16
Kevin Kaske <kevinkaske@gmail.com>
2012-12-06 09:47:01 -0600

    Fixed last commit

--- lib/devise-basecamper/devise/models/authenticatable.rb
+++ lib/devise-basecamper/devise/models/authenticatable.rb
@@ -3,8 +3,8 @@ module Devise
     module Authenticatable
       module ClassMethods
         def find_for_authentication(conditions={})
-          conditions.delete(:subdomain)
-          where(conditions).where(["account_id in (select account_id from accounts where subdomain = :value)", { :value => request.subdomains.first }]).first
+          first_subdomain = conditions.delete(:subdomain).split(".").first
+          where(conditions).where(["account_id in (select account_id from accounts where subdomain = :value)", { :value => first_subdomain }]).first
         end
       end
     end
